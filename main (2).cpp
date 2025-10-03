f6a6f4f3dc02b67e34cb425a2d5511294310b84b
H1dra5-sys <H1dra5@yandex.ru>
2025-10-03 10:47:15 +0700

    Add files via upload

--- main (2).cpp
+++ main (2).cpp
@@ -8,72 +8,29 @@
 
 using namespace std;
 int main() {
-    
-  double a, W, pow1, step1, step2,  step3,  step4,  step5, Pe, Nu, d;
 
-  pow1 = 3.0 * pow(10.0,3.0);
+  double a, W, pow1, Pe, Nu, d;
+  int i;
+
+  pow1 = 3.0 * pow(10.0, 3.0);
   cout << "d =";
   cin >> d; 
   cout << "a =";
   cin >> a;
-  
-  for(double W = 10.0; W < 20.0; W += 10.0) {
-    Pe = W * d / a;
-    if (Pe > pow1) {
-      Nu = 3.4 + 0.014 * pow(Pe, 0.8);
-    }
-        else {
-          Nu = 5.0 + 0.021 * Pe;
-        }
-        
-  cout << "first" << endl
-       << "Nu = " << Nu << endl
-       << "Pe = " << Pe << endl;
-  }
 
-  for(double W = 20.0; W < 50.0; W += 30.0) {
+  for(i = 0; i < 6; i += 1) {
+    cout << "W =";
+    cin >> W;
     Pe = W * d / a;
     if (Pe > pow1) {
       Nu = 3.4 + 0.014 * pow(Pe, 0.8);
+    } else {
+      Nu = 5.0 + 0.021 * Pe;
     }
-        else {
-          Nu = 5.0 + 0.021 * Pe;
-        }
+     
+     cout << "Nu = " << Nu << endl
+         << "Pe = " << Pe << endl;
   }
-  
-  for(double W = 50.0; W < 100.0; W += 50.0) {
-    Pe = W * d / a;
-    if (Pe > pow1) {
-      Nu = 3.4 + 0.014 * pow(Pe, 0.8);
-    }
-        else {
-          Nu = 5.0 + 0.021 * Pe;
-        }
-  }
-  
-  for(double W = 100.0; W < 200.0; W += 100.0) {
-    Pe = W * d / a;
-    if (Pe > pow1) {
-      Nu = 3.4 + 0.014 * pow(Pe, 0.8);
-    }
-        else {
-          Nu = 5.0 + 0.021 * Pe;
-        }
-  }
-  
-  for(double W = 200.0; W < 800.0; W += 300.0) {
-    Pe = W * d / a;
-    if (Pe > pow1) {
-      Nu = 3.4 + 0.014 * pow(Pe, 0.8);
-    }
-        else {
-          Nu = 5.0 + 0.021 * Pe;
-        }
-  }
-
-  cout << "last"  << endl
-       << "Nu = " << Nu << endl
-       << "Pe = " << Pe << endl;
 }
 
   
