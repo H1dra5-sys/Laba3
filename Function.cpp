482de50025e99c7a1c2c644f66d1022ad1dd38d1
H1dra5-sys <H1dra5@yandex.ru>
2025-10-20 00:56:33 +0700

    Add files via upload

--- Function.cpp
+++ Function.cpp
@@ -7,98 +7,53 @@
 
 using namespace std;
 
+int findMax(int arr[], int size) {
+    int maxVal = arr[0];
+    for (int arrayElement = 1; arrayElement < size; ++arrayElement) {
+        if (arr[arrayElement] > maxVal) {
+            maxVal = arr[arrayElement];
+        }
+    }
+    return maxVal;
+}
+
 int main() {
     
-  int  numberOfArrayxElements, numberOfArrayyElements, numberOfArrayzElements, array [numberOfArrayxElements], arrayElement, theSecondElementOfTheArray, maximumElementOfTheArrayX, maximumElementOfTheArrayY, maximumElementOfTheArrayZ;
-  
-  cout << "Enter numberOfArrayxElements" << endl;
-  cin >> numberOfArrayxElements;
+  int  size, arr[size], arrayElement;
+    
+  // First array
+  cout << "Number of array X Elements: ";
+  cin >> size;
   cout << endl;
-  
-  cout << "Enter numberOfArrayyElements" << endl;
-  cin >> numberOfArrayyElements;
-  cout << endl;
-  
-  cout << "Enter numberOfArrayzElements" << endl;
-  cin >> numberOfArrayzElements;
-  cout << endl;
-  
-  maximumElementOfTheArrayX = 0;
-  maximumElementOfTheArrayY = 0;
-  maximumElementOfTheArrayZ = 0;
-
-  cout << "Enter arrayX Elements:" << endl;
-  for (arrayElement = 0; arrayElement < numberOfArrayxElements; ++arrayElement) {
-    cin >> array [arrayElement];
+  cout << "arrayElement: \n";
+  for (int arrayElement = 0; arrayElement < size; ++arrayElement) {
+    cin >> arr[arrayElement];
   }
+  int maxElementX = findMax(arr, size);
+  cout << "Maximum element of the array X: " << maxElementX << endl;
   cout << endl;
-  cout << "arrayX elements: ";
-  for (arrayElement = 0; arrayElement < numberOfArrayxElements; ++arrayElement) {
-    cout << array [arrayElement] << ", ";
+    
+  // Second array    
+  cout << "Number of array Y Elements: : ";
+  cin >> size;
+  cout << endl;
+  cout << "arrayElement: \n";
+  for (int arrayElement = 0; arrayElement < size; ++arrayElement) {
+    cin >> arr[arrayElement];
   }
+  int maxElementY = findMax(arr, size);
+  cout << "Maximum element of the array Y: " << maxElementY << endl;
   cout << endl;
-  for (arrayElement = 0; arrayElement < numberOfArrayxElements; ++arrayElement) {
-    for (theSecondElementOfTheArray = arrayElement + 1; theSecondElementOfTheArray < numberOfArrayxElements; ++theSecondElementOfTheArray) {
-      if (array [arrayElement] >  maximumElementOfTheArrayX) {
-        maximumElementOfTheArrayX = array [arrayElement];
-      }  else {
-           maximumElementOfTheArrayX = maximumElementOfTheArrayX;
-      }
-    }
+    
+  // Third array
+  cout << "Number of array Z Elements: : ";
+  cin >> size;
+  cout << endl;
+  cout << "arrayElement: \n";
+  for (int arrayElement = 0; arrayElement < size; ++arrayElement) {
+    cin >> arr[arrayElement];
   }
+  int maxElementZ = findMax(arr, size);
+  cout << "Maximum element of the array Z: " << maxElementZ << endl;
   cout << endl;
-  cout << "Maximum element of the arrayX" << endl;
-  cout << maximumElementOfTheArrayX;
-  cout << endl;
-  
-  cout << endl;
-  cout << "Enter arrayY Elements:" << endl;
-  for (arrayElement = 0; arrayElement < numberOfArrayyElements; ++arrayElement) {
-    cin >> array [arrayElement];
-  }
-  cout << endl;
-  cout << "arrayY elements: ";
-  for (arrayElement = 0; arrayElement < numberOfArrayyElements; ++arrayElement) {
-    cout << array [arrayElement] << ", ";
-  }
-  cout << endl;
-  for (arrayElement = 0; arrayElement < numberOfArrayyElements; ++arrayElement) {
-    for (theSecondElementOfTheArray = arrayElement + 1 ;theSecondElementOfTheArray < numberOfArrayyElements; ++theSecondElementOfTheArray) {
-      if (array [arrayElement] > maximumElementOfTheArrayY) {
-        maximumElementOfTheArrayY = array [arrayElement];
-      }  else {
-           maximumElementOfTheArrayY = maximumElementOfTheArrayY;
-      }
-    }
-  }
-  cout << endl;
-  cout << "Maximum element of the arrayY" << endl;
-  cout << maximumElementOfTheArrayY;
-  cout << endl;
-  
-  cout << endl;
-  cout << "Enter arrayY Elements:" << endl;
-  for (arrayElement = 0; arrayElement <numberOfArrayzElements; ++arrayElement) {
-    cin >> array [arrayElement];
-  }
-  cout << endl;
-
-  cout << "arrayY elements: ";
-  for (arrayElement = 0; arrayElement < numberOfArrayzElements; ++arrayElement) {
-    cout << array [arrayElement] << ", ";
-  }
-  cout << endl;
-
-  for (arrayElement = 0; arrayElement < numberOfArrayzElements; ++arrayElement) {
-    for (theSecondElementOfTheArray = arrayElement + 1; theSecondElementOfTheArray < numberOfArrayzElements; ++theSecondElementOfTheArray) {
-      if (array [arrayElement] > maximumElementOfTheArrayZ) {
-        maximumElementOfTheArrayZ = array [arrayElement];
-      }  else {
-           maximumElementOfTheArrayZ = maximumElementOfTheArrayZ;
-      }
-    }
-  }
-  cout << endl;
-  cout << "Maximum element of the arrayZ" << endl;
-  cout << maximumElementOfTheArrayZ;
 }
